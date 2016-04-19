3d528f45aed55f325fdad7122581eaef9feeb184
andrewbrownphd <awbrown@uab.edu>
2016-04-19 13:27:30 -0500

    Fixed skipPattern.js

--- inst/templates/skipPattern.js
+++ inst/templates/skipPattern.js
@@ -6,8 +6,12 @@ function skipPattern(deps,tf){
 			/*As long as it's not the first index*/
 			if(indx !== 0) {
 				/*Select the defaults and disable the inputs*/
-				$('input:radio[name=' + deps[indx][0] + '][value=\'' + deps[indx][1] + '\']').prop('checked',tf);
 				$('input:radio[name=' + deps[indx][0] + ']').prop('disabled',tf);
+				$('input:radio[name=' + deps[indx][0] + '][value=\'' + deps[indx][1] + '\']').prop('checked',tf);
+				/*Make sure selected radio is not disabled*/
+				if(tf) {
+					$('input:radio[name=' + deps[indx][0] + '][value=\'' + deps[indx][1] + '\']').prop('disabled',false);
+				}
 			};
 		}
 	)
