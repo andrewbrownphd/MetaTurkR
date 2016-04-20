84a4b43c9c425e95ca58e2c61dd6c8f3a9f77b6c
andrewbrownphd <awbrown@uab.edu>
2016-04-20 18:31:43 -0500

    Updates to MTGetHITSet and MTGetHITTypeResults. MTGetHITTypeResults now actually calls 'GetAssignments' with the 'hit=' instead of 'hit.type=' because of errors when there were HITs with no assignments completed. Also added a function to only get 'incomplete' HITs, which should expedite interim evaluations (e.g., updating HIT-based qualifications).

--- R/turkFunctions.R
+++ R/turkFunctions.R
@@ -64,7 +64,9 @@ MTGetHITSet <- function(HITTypeId,sandbox=TRUE)
 #'
 #' @param HITTypeId REQUIRED. The HITTypeId for the HITs to be returned.
 #' @param reward The amount in dollars that microworkers are rewarded. This is used as a convenient means of returning
-#' the average hourly rate.
+#' the average hourly rate. Not used when only looking at an incomplete set.
+#' @param resultSet Defines the set of assignments to return: \code{"full"} gets all;
+#' \code{"incomplete"} only gets non-reviewed assignments.
 #' @param sandbox Default is \code{TRUE}. Tells the functions whether to operate on the
 #' live or sandbox Mechanical Turk sites.
 #'
@@ -73,20 +75,36 @@ MTGetHITSet <- function(HITTypeId,sandbox=TRUE)
 #'
 MTGetHITTypeResults <- function(HITTypeId = NULL,
                                 reward=NULL,
+                                resultSet="full",
                                 sandbox=TRUE)
 {
   if(is.null(HITTypeId)) stop("HITTypeId is required.")
-  #!#REDO THIS WITH AN OPTION TO ONLY GET INCOMPLETE HITS#!#
+  if(!(resultSet %in% c("full","incomplete"))) stop("resultSet must be either 'full' or 'incomplete'")
   HITSet <- MTGetHITSet(HITTypeId=HITTypeId,
                         sandbox=sandbox)
 
-  results <- MTurkR::GetAssignments(hit.type = HITTypeId,sandbox=sandbox)
-  if(!is.null(reward))  MTHourlyRate(results,reward)
-  results <- merge(HITSet,results[,-which(colnames(results) == "HITTypeId")])
+  if(resultSet == "incomplete"){
+    HITSet <- HITSet[which(as.numeric(HITSet$NumberOfAssignmentsPending) +
+                             as.numeric(HITSet$NumberOfAssignmentsAvailable) +
+                             as.numeric(HITSet$NumberOfAssignmentsCompleted) !=
+                             as.numeric(HITSet$MaxAssignments)),]
+    #Get assignments
+    results <- MTurkR::GetAssignments(hit = HITSet$HITId, sandbox=sandbox)
+    if(!is.null(reward)) message("Only incomplete HITs retrieved; ignoring reward.")
+  } else {
+    #Do any of the HITs have no assignments?
+    none <- HITSet$NumberOfAssignmentsAvailable == HITSet$MaxAssignments
+    #Get assignments
+    results <- MTurkR::GetAssignments(hit = HITSet$HITId[!none],sandbox=sandbox)
+    if(!is.null(reward))  MTHourlyRate(results,reward)
+  }
+
+  results <- merge(HITSet,results,by="HITId")
 
   return(results)
 }
 
+#----------------------------------
 MTHourlyRate <- function(results,reward)
 {
   hourlyRate <- (3600/mean(results$SecondsOnHIT[which(results$AssignmentStatus != "Rejected")]))*reward
