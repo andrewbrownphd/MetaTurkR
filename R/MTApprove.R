39284a3286536d837bd31e701c38630f2eaac728
andrewbrownphd <awbrown@uab.edu>
2016-07-18 15:36:57 -0500

    Made MTApprove consistent with MTCount.

--- R/MTApprove.R
+++ R/MTApprove.R
@@ -8,12 +8,29 @@
 #' @param feedback The response to give to the worker for approving their assignment.
 #' Default is \code{"Thank you."}.
 #' @param sandbox Logical. Whether to use the sandbox (\code{TRUE}) or not; default is \code{TRUE}.
+#' @param outType String, case insensitive. Either set to \code{"approved"} or \code{"full"}.
+#' If \code{"approved"}, only the newly counted subset will be returned. Default is \code{"full"}.
+
+#' @return If \code{outType = "full"}, function returns the original object with the column
+#' \code{AssignmentStatus} updated with \code{'ApprovedLocal'} if newly approved.
+#' If \code{outType="approved"}, function returns only the portion of the object newly approved, with
+#' the \code{AssignmentStatus} column appropriately updated.
 
 MTApprove <- function(results=NULL,
                       feedback = "Thank you.",
                       sandbox=TRUE)
 {
   if(is.null(results)) stop("No results specified.")
+
+  outType <- tolower(outType)
+  if(!(outType %in% c("full","approved"))) {
+    warning("'outType' not specified. Defaulting to 'approved'.")
+    outType <- "approved"
+  }
+
+  #Back up results object for output if appropriate
+  if(outType == "full") resultsAll <- results
+
   #Tries to approve them one at a time to catch errors; slow, but not slower than dealing
   #with errors midstream
   for(r in 1:nrow(results)){
@@ -53,4 +70,14 @@ MTApprove <- function(results=NULL,
     }
     )
   }
+
+  if(outType == "full")
+  {
+    #Fix up the 'resultsAll' object with 'Approved' information
+    approvedAssignments <- results$AssignmentId[which(results$AssignmentStatus == "ApprovedLocal")]
+    resultsAll$AssignmentStatus[which(resultsAll$AssignmentId %in% approvedAssignments)] <- "ApprovedLocal"
+    return(invisible(resultsAll))
+  }
+  if(outType == "approved")  return(invisible(results))
+
 }
